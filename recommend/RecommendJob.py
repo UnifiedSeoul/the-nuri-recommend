9f9c752cbf0e661ab700c4bc85558e22da75fe5b
YounginYoon <3ylsjlsj@gmail.com>
2024-05-06 14:43:01 +0900

    first commit

---
+++ recommend/RecommendJob.py
@@ -0,0 +1,146 @@
+# 설치
+# pip install xmltodict
+# pip install transformers
+# pip install --ignore-installed sentence-transformers==2.7.0
+# pip install tf-keras
+
+import pymysql
+import lxml
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import time
+import math
+import xmltodict, json
+from datetime import datetime
+from pytz import timezone
+import pickle
+from transformers import pipeline
+from sentence_transformers import SentenceTransformer, util
+from sklearn.metrics.pairwise import cosine_similarity
+import requests
+import os
+
+def getJobList():
+    host = 'thenuridatabase.cdagciaaigw0.ap-northeast-2.rds.amazonaws.com'
+    port = 3306
+    user = 'admin'
+    password = 'nuri1234'
+    database = 'thenuri'
+
+    connection = pymysql.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database=database,
+        charset="utf8"
+    )
+
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+    cursor.execute("SELECT * FROM thenuri.job")
+
+    result = cursor.fetchall()
+    jobData = pd.DataFrame(result)
+    connection.close()
+    return jobData
+
+# 임시 공고 전처리
+def extractZipCode(text):
+    zipCode = text.split()
+    return zipCode[0]
+
+def embeddingJobList(jobs):
+    print('\nstart embeddingJobList...\n')
+    # # 서울 공고 추출을 위한 우편 번호 추가 (01000~01200)
+    # jobs['zip_code'] = jobs['place_detail_address'].apply(extract_zipCode)
+    
+    # # 오늘 날짜 이전의 마감 공고 제거
+    # today = datetime.now(timezone('Asia/Seoul')).strftime('%Y%m%d')
+
+    # seoul_jobs = jobs.loc[(jobs['zip_code'].str.startswith('0')) & (jobs['to_acceptance_date'] >= today)]
+    # seoul_jobs.reset_index(drop=True, inplace=True)
+
+    # 공고 제목을 영어로 번역
+    recruitment_title = list(jobs['recruitment_title'])
+    pipe_trans = pipeline("translation", model="circulus/kobart-trans-ko-en-v2", max_length=200)
+    
+    print('\n...translating...\n')
+    translated_title_pipe = pipe_trans(recruitment_title)
+
+    translated_title = [title['translation_text'] for title in translated_title_pipe if 'translation_text' in title]
+
+    # 공고 제목 토큰화 및 벡터화
+    MODEL_ID = 'sentence-transformers/all-MiniLM-L6-v2'
+    model = SentenceTransformer(MODEL_ID)
+    
+    print('\n...encoding...\n')
+    title_embeddings = model.encode(translated_title)
+
+    # 로컬에 저장
+    print('\n...saving...\n')
+    file_dir = os.path.dirname(__file__)
+    title_embedding_pos = os.path.join(file_dir, 'data', 'TitleEmbedding.npy')
+    np.save(title_embedding_pos, title_embeddings)
+    
+    jobs_pos = os.path.join(file_dir, 'data', 'Jobs.csv')
+    jobs.to_csv(jobs_pos, index=None)
+
+    embedding_model_pos = os.path.join(file_dir, 'model', 'EmbeddingModel.pkl')
+    with open(embedding_model_pos, 'wb') as f:
+        pickle.dump(model, f)
+
+    translation_model_pos = os.path.join(file_dir, 'model', 'TranslationModel.pkl')
+    with open(translation_model_pos, 'wb') as f:
+        pickle.dump(pipe_trans, f)
+
+def getRecommendation(user_info):
+    # 추천 시작
+    # 입력 받은 유저 정보 토큰화 및 벡터화
+    
+    file_dir = os.path.dirname(__file__)
+    title_embedding_pos = os.path.join(file_dir, 'data', 'TitleEmbedding.npy')
+    TitleEmbedding = np.load(title_embedding_pos)
+    
+    jobs_pos = os.path.join(file_dir, 'data', 'Jobs.csv')
+    Jobs = pd.read_csv(jobs_pos)
+
+    embedding_model_pos = os.path.join(file_dir, 'model', 'EmbeddingModel.pkl')
+    with open(embedding_model_pos, 'rb') as f:
+        EmbeddingModel = pickle.load(f)
+        
+    translation_model_pos = os.path.join(file_dir, 'model', 'TranslationModel.pkl')
+    with open(translation_model_pos, 'rb') as f:
+        TranslationModel = pickle.load(f)
+    
+    trans_input = TranslationModel(user_info)
+    encode_input = trans_input[0]['translation_text']
+    embedding_input = EmbeddingModel.encode(encode_input)
+
+    # 코사인 유사도 측정
+    cos_sim = []
+    for i, emb in enumerate(TitleEmbedding):
+        cos_sim.append((i, util.cos_sim(embedding_input, emb)[0].item()))
+
+    cos_sim = sorted(cos_sim, key=lambda x:-x[1])
+    cos_sim = [x for x in cos_sim if x[1] >= 0]
+    top_1 = cos_sim[0][0]
+
+    job_id_list = Jobs['job_id'].to_list()
+    print(Jobs.loc[Jobs.index==top_1, ['company_name', 'recruitment_title']])
+    return job_id_list[top_1]
+
+def recommendation(data):
+    results = []
+    for d in data:
+        input_recommend = d['jobPlace'] + " " + d['jobSpecific']
+        rec_id = getRecommendation(input_recommend)
+        results.append(rec_id)
+    
+    return results
+
+# jobs = getJobList()
+# print('\nfinish getJobList...\n')
+# embeddingJobList(jobs)
---
+++ server/app.py
@@ -0,0 +1,34 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from flask import Flask, request, jsonify
+from recommend.RecommendJob import *
+
+app = Flask(__name__)
+
+@app.route('/api/recommend', methods=['POST'])
+def get_user_data():
+    if request.method == 'POST':
+        request_data = request.get_json()
+        userJobInfoList = request_data.get("userJobInfoList")
+        recommend_jobId = recommendation(userJobInfoList)
+        if recommend_jobId:
+            # api 요청
+            url = 'http://127.0.0.1:5000/api/recommend/result'
+            json_data = {"recommendJobId": recommend_jobId}
+            post_response = requests.post(url, json=json_data)
+            if post_response.status_code == 200:
+                return post_response.text, 200
+            else:
+                print("POST 요청에 실패했습니다. 상태 코드:", post_response.status_code)
+        
+        return ' ', post_response.status_code
+
+@app.route('/api/recommend/result', methods=['POST'])
+def check_recommend():
+    request_data = request.get_json()
+    return request_data
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
