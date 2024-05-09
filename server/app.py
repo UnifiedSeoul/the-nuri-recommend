7319288db2dc146d0ef4735eaa56aab9c991df61
Yoonpyo Hong <yoon84268426@gmail.com>
2024-05-09 18:25:34 +0900

    fix : log check

--- server/app.py
+++ server/app.py
@@ -4,10 +4,21 @@ from waitress import serve
 from flask import Flask, request, jsonify, abort
 from recommend.RecommendJob import *
 from flask_cors import CORS
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
 CORS(app, resources={r"/api/*": {"origins": "http://43.200.89.34"}})
+
+def setup_logging():
+    handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=3)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.INFO)
 
 @app.route('/api/recommend', methods=['POST'])
 def get_user_data():
@@ -27,4 +38,5 @@ def check_recommend():
 
 
 if __name__ == '__main__':
+    setup_logging()
     serve(app, host='0.0.0.0', port=5000)
